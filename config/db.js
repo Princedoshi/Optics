2e0489a99dec8c094343643a3ff46918ea95a34e
Prince Doshi <pdoshi135@gmail.com>
2025-03-16 19:39:03 +0530

    push

--- config/db.js
+++ config/db.js
@@ -4,7 +4,6 @@ require("dotenv").config(); // Load .env variables
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
-        console.log("MongoDB Connected");
     } catch (error) {
         console.error("MongoDB Connection Error:", error);
         process.exit(1);
--- index.js
+++ index.js
@@ -44,4 +44,3 @@ app.delete("/api/clear-db", async (req, res) => {
     }
 });
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
